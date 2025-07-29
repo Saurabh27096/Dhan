6234a41ccc6dec57d35549de25f748a90e0b3329
saurabh <saurabh10032001.com>
2025-07-29 13:54:14 +0530

    first commit

---
+++ src/Encaptulation/main.java
@@ -0,0 +1,9 @@
+package Encaptulation;
+
+public class main {
+    public static void main(String[] args) {
+        temp t = new temp();
+        t.setName("yadav");
+        System.out.println(t.getName() + Math.max(2 , 3 ));
+    }
+}
